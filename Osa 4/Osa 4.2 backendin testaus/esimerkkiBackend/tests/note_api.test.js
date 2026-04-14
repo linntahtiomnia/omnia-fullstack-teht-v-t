a6cdb8ca63e925b2cb461c2eae0102ea0d6afb41
linntahti <linn.tahti@omnia.fi>
2026-04-14 12:29:39 +0300

    Fix tests

--- Osa 4/Osa 4.2 backendin testaus/esimerkkiBackend/tests/note_api.test.js
+++ Osa 4/Osa 4.2 backendin testaus/esimerkkiBackend/tests/note_api.test.js
@@ -1,3 +1,7 @@
+/*
+Tämä tiedosto sisältää testit muistiinpanojen REST API:lle.
+Testit on kirjoitettu Node.js:n omalla testauskirjastolla, ja käyttäen supertest-kirjastoa HTTP-pyyntöjen tekemiseen.
+*/
 const assert = require('node:assert')
 const { test, after, before, beforeEach } = require('node:test')
 const supertest = require('supertest')
@@ -8,16 +12,20 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
+// Ennen testien suorittamista tyhjennetään tietokanta ja lisätään sinne tarvittavat taulut sync-komennolla.
 before(async () => {
+  await sequelize.drop()
   await sequelize.sync({ force: true })
 })
 
+// Ennen jokaista testiä poistetaan kaikki rivit note-taulusta ja lisätään sinne testidataa.
 beforeEach(async () => {
   await Note.destroy({ where: {} }) // Tyhjennetään taulukko ennen jokaista testiä
-  await Note.create(helper.initialNotes[0])
+  await Note.create(helper.initialNotes[0]) // Testidata löytyy test_helper.js-tiedostosta
   await Note.create(helper.initialNotes[1])
 })
 
+// Testataan, että muistiinpanot palautuvat JSON-muodossa, kun tehdään HTTP GET-pyyntö /api/notes-polkuun.
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
@@ -25,12 +33,15 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+// Testataan, että kaikki muistiinpanot palautuvat, kun tehdään HTTP GET-pyyntö /api/notes-polkuun.
+// Tarkistetaan, että muistiinpanoja palautuu oikea määrä.
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
   assert.strictEqual(response.body.length, helper.initialNotes.length)
 })
 
+// Testataan, että tietty muistiinpano löytyy palautetuista muistiinpanoista.
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
@@ -38,6 +49,7 @@ test('a specific note is within the returned notes', async () => {
   assert(contents.includes('HTML is easy'))
 })
 
+// Testataan, että uusi muistiinpano voidaan lisätä onnistuneesti HTTP POST-pyynnöllä /api/notes-polkuun.
 test('a valid note can be added ', async () => {
   const newNote = {
     content: 'async/await simplifies making async calls',
@@ -51,24 +63,26 @@ test('a valid note can be added ', async () => {
     .expect('Content-Type', /application\/json/)
 
   const notesAtEnd = await helper.notesInDb()
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1) // Tarkistetaan, että muistiinpanojen määrä on kasvanut yhdellä
 
   const contents = notesAtEnd.map((n) => n.content)
-  assert(contents.includes('async/await simplifies making async calls'))
+  assert(contents.includes('async/await simplifies making async calls')) // Tarkistetaan, että uusi muistiinpano löytyy tietokannasta
 })
 
+// Testataan, että muistiinpano, jolla ei ole content-kenttää, ei lisätä tietokantaan.
 test('note without content is not added', async () => {
   const newNote = {
     important: true,
   }
 
-  await api.post('/api/notes').send(newNote).expect(400)
+  await api.post('/api/notes').send(newNote).expect(400) // Tarkistetaan, että palvelin palauttaa 400 Bad Request -virheen
 
   const notesAtEnd = await helper.notesInDb()
 
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length) // Tarkistetaan, että muistiinpanojen määrä ei ole kasvanut
 })
 
+// Testataan, että tietty muistiinpano voidaan hakea onnistuneesti HTTP GET-pyynnöllä /api/notes/:id-polkuun.
 test('a specific note can be viewed', async () => {
   const notesAtStart = await helper.notesInDb()
   const noteToView = notesAtStart[0]
@@ -78,9 +92,10 @@ test('a specific note can be viewed', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-  assert.deepStrictEqual(resultNote.body, noteToView)
+  assert.deepStrictEqual(resultNote.body, noteToView) // Kun vertaillaan olioita, käytetään deepStrictEqual-metodia, joka tarkistaa, että olioiden sisältö on sama, eikä vain viittaukset olioihin
 })
 
+// Testataan, että muistiinpano, jolla on olemassa oleva id, voidaan poistaa onnistuneesti HTTP DELETE-pyynnöllä /api/notes/:id-polkuun.
 test('a note can be deleted', async () => {
   const notesAtStart = await helper.notesInDb()
   const noteToDelete = notesAtStart[0]
@@ -90,11 +105,12 @@ test('a note can be deleted', async () => {
   const notesAtEnd = await helper.notesInDb()
 
   const contents = notesAtEnd.map((n) => n.content)
-  assert(!contents.includes(noteToDelete.content))
+  assert(!contents.includes(noteToDelete.content)) // Tarkistetaan, että poistetun muistiinpanon sisältö ei enää löydy tietokannasta
 
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1) // Tarkistetaan, että muistiinpanojen määrä on vähentynyt yhdellä
 })
 
+// Lopuksi suljetaan tietokantayhteys, jotta testit eivät jää roikkumaan.
 after(async () => {
   await sequelize.close()
 })
--- Osa 4/Osa 4.2 backendin testaus/esimerkkiBackend/tests/test_helper.js
+++ Osa 4/Osa 4.2 backendin testaus/esimerkkiBackend/tests/test_helper.js
@@ -1,5 +1,11 @@
+/*
+Tässä tiedostossa määritellään apufunktioita, joita käytetään note_api.test.js-tiedostossa.
+Näitä funktioita käytetään testien suorittamisen yhteydessä tietokannan tilan tarkistamiseen
+ja muistiinpanojen luomiseen testitilanteissa.
+*/
 const Note = require('../models/note')
 
+// Kovakoodattu testidata
 const initialNotes = [
   {
     content: 'HTML is easy',
@@ -11,6 +17,8 @@ const initialNotes = [
   },
 ]
 
+// Funktio, joka luo tietokantaan muistiinpanon, poistaa sen ja palauttaa sen id:n.
+// Tätä käytetään testaamaan, miten palvelin käyttäytyy saadessaan parametrina id:n, jota ei löydy tietokannasta.
 const nonExistingId = async () => {
   const note = await Note.create({ content: 'willremovethissoon' })
   const id = note._id.toString()
@@ -19,6 +27,7 @@ const nonExistingId = async () => {
   return id
 }
 
+// Funktio, joka hakee tietokannasta kaikki muistiinpanot ja palauttaa ne JSON-muodossa.
 const notesInDb = async () => {
   const notes = await Note.findAll()
   return notes.map((note) => note.toJSON())
